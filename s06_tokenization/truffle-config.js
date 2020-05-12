e90720ec10df4898040934d7951b80c5b308dcf3
Edson Ramirez <edsonrmz.dev@gmail.com>
2020-05-12 00:48:22 -0400

    added kyc contract and made us of tokens

--- s06_tokenization/truffle-config.js
+++ s06_tokenization/truffle-config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+require('dotenv').config({path: "/Users/edson/Desktop/Blockchain-projects/First-Token/.env"});
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const MetaMaskAccountIndex = 0;
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -9,6 +12,24 @@ module.exports = {
       port: 7545,
       network_id: "*",
       host: "127.0.0.1"
+    },
+    ganache_local: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "http://127.0.0.1:7545", MetaMaskAccountIndex)
+      },
+      network_id: 5777
+    },
+    ropsten_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://ropsten.infura.io/v3/bb6f83daa78f4d3d927576eeba463ee2", MetaMaskAccountIndex)
+      },
+      network_id: 3
+    },
+    goerli_infura: {
+      provider: function() {
+        return new HDWalletProvider(process.env.MNEMONIC, "https://goerli.infura.io/v3/bb6f83daa78f4d3d927576eeba463ee2", MetaMaskAccountIndex)
+      },
+      network_id: 5
     }
   },
   compilers: {
